e29d475f4eef3ac31458f2788c9b13913e5520e8
Sean <sean.yasno@gmail.com>
2019-07-15 12:10:23 +0300

    Some stuff

---
+++ Assets/Scripts/TotalMobsManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TotalMobsManager : MonoBehaviour
+{
+
+    public Transform[] spawnLocations;
+
+    private SanityManager sanityManager;
+
+    private void Awake() {
+        sanityManager = GameObject.FindGameObjectWithTag("Player").GetComponent<SanityManager>();
+        if (sanityManager == null)
+            Debug.LogError("PLAYER IS MISSING");
+    }
+
+    private void Update() {
+        
+    }
+
+}
+
+[CreateAssetMenu(fileName ="mob name", menuName ="Mob")]
+public class Mob : ScriptableObject {
+
+    public GameObject mobPrefab;
+    public int minSpawnAmount;
+    public int maxSpawnAmount;
+
+}
+

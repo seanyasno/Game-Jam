d3afe2fe7c0b0782a6f255b0ecd8a37790b8d6e2
Sean <sean.yasno@gmail.com>
2019-07-15 14:36:20 +0300

    ok

---
+++ Assets/Scripts/Player/ChangeForm.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeForm : MonoBehaviour
+{
+    
+    [Header("Components")]
+    [SerializeField] private Sprite firstForm = null;
+    [SerializeField] private Sprite secondForm = null;
+    
+    [Header("Global Data")]
+    [SerializeField] public bool hasTransformed = false;
+
+    private void Start(){
+        hasTransformed = false;
+        GetComponent<SpriteRenderer>().sprite = firstForm;
+    }
+
+    private void Update(){
+        if (Input.GetKeyDown(KeyCode.X)){
+
+            switch(hasTransformed){
+
+                case true:
+                    changeSprite(firstForm, true);
+                    break;
+
+                case false:
+                     changeSprite(secondForm, false);
+                    break;
+
+            }
+            hasTransformed = !hasTransformed;
+        }
+    }
+
+    private void changeSprite(Sprite form, bool enablePlayerMovement){
+
+        GetComponent<SpriteRenderer>().sprite = form;
+        GetComponent<PlayerMovement>().enabled = enablePlayerMovement;
+
+    }
+}

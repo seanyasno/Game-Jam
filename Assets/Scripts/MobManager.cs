6b001200f34602dc3d5eaf3976d115827f0cd533
Sean <sean.yasno@gmail.com>
2019-07-15 19:15:11 +0300

    Camera Shake plus Damage by range have been added

--- Assets/Scripts/MobManager.cs
+++ Assets/Scripts/MobManager.cs
@@ -4,15 +4,31 @@ using UnityEngine;
 
 public class MobManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+
+    public float damangeReductionFactor = 0.9f;
+    [Range(0f, 50f)] public float range;
+
+    private SanityManager sanityManager;
+    private GameObject player;
+
+    private void Awake() {
+        player = GameObject.FindGameObjectWithTag("Player");
+        sanityManager = player.GetComponent<SanityManager>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    private void Update() {
+        float distanceFromPlayer = Vector3.Distance(transform.position, player.transform.position);
+
+        if (distanceFromPlayer <= range) {
+
+            float normalizedDistance = distanceFromPlayer / range;
+            float reductionFactor = damangeReductionFactor * (1 - normalizedDistance) + normalizedDistance;
+
+            print(reductionFactor);
+
+            sanityManager.DecreaseSanity(reductionFactor * 0.1f);
+        }
     }
+
+
 }
